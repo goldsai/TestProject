dec3e2962939061496b0f5c4b7878268fab2abc4
SAI <SAI@DESKTOP-IHATE0N>
2019-08-08 01:17:52 +0300

    First chenge

---
+++ My/src/MyCls.java
@@ -0,0 +1,39 @@
+
+public class MyCls {
+	private int val;
+	private String name;
+	/**
+	 * @return the val
+	 */
+	public int getVal() {
+		return val;
+	}
+	/**
+	 * @param val the val to set
+	 */
+	public void setVal(int val) {
+		this.val = val;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @param val
+	 * @param name
+	 */
+	public MyCls(int val, String name) {
+		//super();
+		this.val = val;
+		this.name = name;
+	}
+	
+}
